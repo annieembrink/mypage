5d59164795a47c97c427ee73a71cef420f3ba456
anniesandberg <90192959+anniesandberg@users.noreply.github.com>
2022-11-02 10:58:55 +0100

    added contact menu

--- code.js
+++ code.js
@@ -1,32 +1,31 @@
 let profPic = document.getElementById('profPic');
 let imgContainer = document.getElementById('imgContainer');
-
-// function imgOnClick() {
-//     imgContainer.style.width = "100%";
-//     imgContainer.style.height = "100%";
-//     imgContainer.style.margin = "0"
-//     imgContainer.style.backgroundColor = "lightgrey";
-//     profPic.style.width = "50%"
-//     imgContainer.style.position = "absolute"
-// }
+let contact = document.getElementById('contact');
 
 // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
+let modal = document.getElementById("myModal");
+let contactModal = document.getElementById("myContactModal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
+let contactClose = document.getElementsByClassName("contactClose")[0];
 
 // When the user clicks on the button, open the modal
 function imgOnClick() {
   modal.style.display = "block";
 }
 
+function contactOnClick() {
+  contactModal.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+
+contactClose.onclick = function() {
+  contactModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
